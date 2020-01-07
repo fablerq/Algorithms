e164ae73e848e73ab3f16ca71b0036beea4e56e5
Anatolii <kenaz1222@gmail.com>
2020-01-07 23:58:18 +0300

    feat: added shell sort in Java

--- Algorithms/Sorting/Shell Sort/ShellSort.java
+++ Algorithms/Sorting/Shell Sort/ShellSort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class InsertionSort {
+class ShellSort {
     public static void main(String[] args) {
         int[] data = { 1, 5, 4, 8, 12, 2, 1, 2, 7, 9 };
         System.out.println("Before: " + Arrays.toString(data));
@@ -10,17 +10,13 @@ class InsertionSort {
 
     public static int[] sort(int[] data) {
         int n = data.length;
-        for (int h = 1; h < N / 3; h /= 3 + 1)
-        {
-            for (int i = h; i < n; i++)
-            {
-                int temp = data[i];
-                int j;
-                for (j = i; j >= h && data[j - h] > temp; j -= h)
-                    data[j] = data[j - h];
-                data[j] = temp;
-            }
-        }
+        for (int i = 1; i < n; i++)
+            for (int j = i; j > 0; j--)
+                if (data[j] < data[j-1]) {
+                    int temp = data[j];
+                    data[j] = data[j-1];
+                    data[j-1] = temp;
+                }
         return data;
     }
 }
